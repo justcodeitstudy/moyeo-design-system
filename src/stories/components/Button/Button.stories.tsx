8fdedd549ee2de89cea8de944c556be4624625f6
ssabi <fhy1930@gmail.com>
2022-10-01 14:24:00 +0900

    chore: 미사용 parameter 제거

--- src/stories/components/Button/Button.stories.tsx
+++ src/stories/components/Button/Button.stories.tsx
@@ -1,12 +1,10 @@
 import { Meta, Story } from "@storybook/react";
 import React from "react";
 import Button, { ButtonProps } from "../../../components/Button";
-import { parameters } from "../../defaultMetaParams";
 
 export default {
   title: "Components/Button",
   component: Button,
-  parameters,
 } as Meta;
 
 const Template: Story<ButtonProps> = (args) => <Button {...args}>버튼</Button>;
