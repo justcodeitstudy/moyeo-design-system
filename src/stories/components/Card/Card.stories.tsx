b989ee478fd8f7037be15955345aa06a24161922
kimnamsun <nsunny0908@naver.com>
2022-10-03 00:39:12 +0900

    :sparkles: feat: Card component

---
+++ src/stories/components/Card/Card.stories.tsx
@@ -0,0 +1,18 @@
+import { Meta, Story } from "@storybook/react";
+import React from "react";
+import { Card, CardProps } from "../../../components/Card/Card";
+
+export default {
+  title: "Components/Card",
+  component: Card,
+} as Meta;
+
+export const Template: Story<CardProps> = (args) => (
+  <Card {...args}>
+    <Card.Content>Card Content</Card.Content>
+    <Card.Action>
+      <hr />
+      <button>Card Action</button>
+    </Card.Action>
+  </Card>
+);
