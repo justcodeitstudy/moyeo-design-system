b6b55dd51f455e86d6e0403b72f20c05c550a3c5
kimnamsun <nsunny0908@naver.com>
2022-10-02 23:31:03 +0900

    :lipstick: style: theme color, typography 설정

--- src/styles/theme/default.ts
+++ src/styles/theme/default.ts
@@ -1,9 +1,95 @@
 const colors = {
-  primary: "#12D4FF",
+  primary: {
+    "50": "#f3fcff",
+    "100": "#e6fafe",
+    "200": "#c1f2fd",
+    "300": "#9beafc",
+    "400": "#51dbf9",
+    "500": "#06cbf7",
+    "600": "#05b7de",
+    "700": "#0598b9",
+    "800": "#047a94",
+    "900": "#036379",
+  },
+  warning: {
+    "50": "#fffbf2",
+    "100": "#fef7e6",
+    "200": "#fdecbf",
+    "300": "#fce099",
+    "400": "#fac94d",
+    "500": "#f8b200",
+    "600": "#dfa000",
+    "700": "#ba8600",
+    "800": "#956b00",
+    "900": "#7a5700",
+  },
+  general: {
+    "50": "#fbfbfb",
+    "100": "#f7f7f7",
+    "200": "#ebebeb",
+    "300": "#dfdfdf",
+    "400": "#c7c7c7",
+    "500": "#afafaf",
+    "600": "#9e9e9e",
+    "700": "#838383",
+    "800": "#696969",
+    "900": "#565656",
+  },
+  typography: {
+    title: "#000000",
+    primary: "#444444",
+    secondary: "#888888",
+    third: "#BFBFBF",
+    disabled: "#D9D9D9",
+  },
+};
+
+const typography = {
+  header1: {
+    fontSize: "32px",
+    fontWeight: "700",
+    lineHeight: "40px",
+  },
+  header2: {
+    fontSize: "24px",
+    fontWeight: "600",
+    lineHeight: "32px",
+  },
+  header3: {
+    fontSize: "20px",
+    fontWeight: "500",
+    lineHeight: "28px",
+  },
+  header4: {
+    fontSize: "16px",
+    fontWeight: "500",
+    lineHeight: "24px",
+  },
+  lg: {
+    fontSize: "20px",
+    fontWeight: "400",
+    lineHeight: "28px",
+  },
+  md: {
+    fontSize: "16px",
+    fontWeight: "400",
+    lineHeight: "24px",
+  },
+  sm: {
+    fontSize: "14px",
+    fontWeight: "400",
+    lineHeight: "22px",
+  },
+  xs: {
+    fontSize: "12px",
+    fontWeight: "400",
+    lineHeight: "20px",
+  },
 };
 
 const theme = {
   colors,
+  typography,
 };
 
 export default theme;
