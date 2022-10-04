a98ec27d473ffc5b343b63c0d752a849bab0eb45
HANYUNSEONG <hanyunseong.dev@gmail.com>
2022-10-04 21:44:17 +0900

    feat: Tooltip 구현

---
+++ src/stories/components/Tooltip/Tooltip.stories.tsx
@@ -0,0 +1,18 @@
+import { Meta, Story } from "@storybook/react";
+import React from "react";
+import Tooltip, { TooltipProps } from "../../../components/Tooltip";
+
+export default {
+  title: "Components/Tooltip",
+  component: Tooltip,
+} as Meta;
+
+const Template: Story<TooltipProps> = (args) => <Tooltip {...args} />;
+
+export const Basic: Story<TooltipProps> = (args) => <Template {...args} />;
+
+Basic.args = {
+  children: <button style={{ padding: "4px 24px" }}>트리거</button>,
+  content: "Tooltip 툴팁",
+  placement: "bottom",
+};
