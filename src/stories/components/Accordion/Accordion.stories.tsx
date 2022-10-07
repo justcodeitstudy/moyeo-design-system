67de0cc7e783d355be6d921693239b8353e58d1a
ssabi <fhy1930@gmail.com>
2022-10-08 01:44:22 +0900

    chore: storybook 빈 태그 제거

--- src/stories/components/Accordion/Accordion.stories.tsx
+++ src/stories/components/Accordion/Accordion.stories.tsx
@@ -9,20 +9,18 @@ export default {
 
 const Template: Story<AccordionProps> = (args) => {
   return (
-    <>
-      <Accordion {...args}>
-        <Accordion.Title>Accordion</Accordion.Title>
-        <Accordion.Content>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Content>
-      </Accordion>
-    </>
+    <Accordion {...args}>
+      <Accordion.Title>Accordion</Accordion.Title>
+      <Accordion.Content>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </Accordion.Content>
+    </Accordion>
   );
 };
 
