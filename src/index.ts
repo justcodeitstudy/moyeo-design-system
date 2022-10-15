2c4f69f222ce3abb6a95d684a4fd8c9f9e23d8ab
Hyosub Song <hyosub@redrocket.co.kr>
2022-10-16 01:15:32 +0900

    feature: GlobalStyle, Theme export

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,7 @@ export { SearchInput } from "components/SearchInput";
 export { Tabs } from "components/Tabs";
 export { TextInput } from "components/TextInput";
 export * from "./components/Modal";
+
+// theme
+export { default as GlobalStyle } from "styles/global";
+export { default as Theme } from "styles/theme";
