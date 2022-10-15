1be3a5931591228907958ffade6f4b82f72620da
ssabi <fhy1930@gmail.com>
2022-10-16 02:41:48 +0900

    fix: typography type 수정

--- src/styles/theme/typography/Typography.ts
+++
@@ -1,5 +0,0 @@
-export type Typography = {
-  fontSize: string;
-  fontWeight: string;
-  lineHeight: string;
-};
--- src/styles/theme/typography/index.ts
+++ src/styles/theme/typography/index.ts
@@ -1,47 +1,52 @@
-import { Typography } from "./Typography";
+import { css, FlattenSimpleInterpolation } from "styled-components";
 
-const header1: Typography = {
-  fontSize: "32px",
-  fontWeight: "700",
-  lineHeight: "40px",
-};
+const header1: FlattenSimpleInterpolation = css`
+  font-size: 32px;
+  font-weight: 700;
+  line-height: 40px;
+`;
 
-const header2: Typography = {
-  fontSize: "24px",
-  fontWeight: "600",
-  lineHeight: "32px",
-};
+const header2: FlattenSimpleInterpolation = css`
+  font-size: 24px;
+  font-weight: 600;
+  line-height: 32px;
+`;
 
-const header3: Typography = {
-  fontSize: "20px",
-  fontWeight: "500",
-  lineHeight: "28px",
-};
-const header4: Typography = {
-  fontSize: "16px",
-  fontWeight: "500",
-  lineHeight: "24px",
-};
-const lg: Typography = {
-  fontSize: "20px",
-  fontWeight: "400",
-  lineHeight: "28px",
-};
-const md: Typography = {
-  fontSize: "16px",
-  fontWeight: "400",
-  lineHeight: "24px",
-};
-const sm: Typography = {
-  fontSize: "14px",
-  fontWeight: "400",
-  lineHeight: "22px",
-};
-const xs: Typography = {
-  fontSize: "12px",
-  fontWeight: "400",
-  lineHeight: "20px",
-};
+const header3: FlattenSimpleInterpolation = css`
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 28px;
+`;
+
+const header4: FlattenSimpleInterpolation = css`
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 24px;
+`;
+
+const lg: FlattenSimpleInterpolation = css`
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 28px;
+`;
+
+const md: FlattenSimpleInterpolation = css`
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 24px;
+`;
+
+const sm: FlattenSimpleInterpolation = css`
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 22px;
+`;
+
+const xs: FlattenSimpleInterpolation = css`
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 20px;
+`;
 
 const typography = {
   header1,
