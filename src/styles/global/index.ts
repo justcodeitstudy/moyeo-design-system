886c6b6997fa5633ec02a379371acd78c702028b
ssabi <fhy1930@gmail.com>
2022-10-18 03:30:56 +0900

    fix: GlobalStyle reset 코드 추가

--- src/styles/global/index.ts
+++ src/styles/global/index.ts
@@ -1,7 +1,21 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
   body {
     font-family: Pretendard, sans-serif;
+  }
+
+  body,div,dl,dt,dd,ul,ol,li,h1,h2,h3,h4,h5,h6,pre,code,form,fieldset,legend,input,button,textarea,p,blockquote,th,td { margin:0; padding:0; }
+  img { border:none; }
+  ol,ul,li { list-style:none; }
+
+  a {
+    color: inherit;
+    cursor: pointer;
+    text-decoration: none;
   }
 `;
