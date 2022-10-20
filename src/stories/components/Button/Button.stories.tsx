1efa489546bab52604e1ded9616ab7d501b6c549
kimnamsun <nsunny0908@naver.com>
2022-10-21 01:10:46 +0900

    :zap: advanced: Button icon 추가

--- src/stories/components/Button/Button.stories.tsx
+++ src/stories/components/Button/Button.stories.tsx
@@ -1,6 +1,9 @@
 import { Meta, Story } from "@storybook/react";
 import React from "react";
 import { Button, ButtonProps } from "components/Button";
+import { Icon } from "components/Icon";
+import styled from "styled-components";
+import { Theme } from "styles/theme";
 
 export default {
   title: "Components/Button",
@@ -12,3 +15,29 @@ const Template: Story<ButtonProps> = (args) => <Button {...args}>버튼</Button>
 export const Default: Story<ButtonProps> = (args) => <Template {...args} />;
 
 Default.args = {};
+
+export const WithIcon = () => {
+  return (
+    <Container>
+      <Button endIcon={<Icon name="write" size={24} />}>
+        모집 글 작성하기
+      </Button>
+      <StyledButton color="general">
+        <Icon name="checkWithCircle" color={Theme.colors.general[600]} />
+      </StyledButton>
+      <Button startIcon={<Icon name="write" />}>모집 글 작성하기</Button>
+    </Container>
+  );
+};
+
+const Container = styled("div")`
+  display: flex;
+
+  button {
+    margin-right: 4px;
+  }
+`;
+
+const StyledButton = styled(Button)`
+  padding: 8px;
+`;
